373120d94e7f7cde90bd2b7092d033e3b9300bd9
Paramita Basu <paramita.das@tcs.com>
2017-05-07 23:16:00 +0530

    Course Project 1 plot1

---
+++ plot1.R
@@ -0,0 +1,15 @@
+
+dataFile <- "./data/household_power_consumption.txt"
+#reading the data file
+consumpData <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+
+#selecting data only from the dates 2007-02-01 and 2007-02-02 
+subsetConsumpData <- consumpData[consumpData$Date %in% c("1/2/2007","2/2/2007"),]
+
+# GlobalActivePower data
+globalActivePower <- as.numeric(subsetConsumpData$Global_active_power)
+
+#Plot the GlobalActivePower data
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)" )
+dev.off()
